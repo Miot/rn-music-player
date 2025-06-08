fb01b4a2d9b4d7d43137b974f4b3fd643199fd66
Miot <imuy@foxmail.com>
2025-06-09 01:25:27 +0800

    feat: adjust to track change in different page

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,6 +1,7 @@
 import { TracksList } from '@/components/TracksList'
 import { screenPadding } from '@/constants/tokens'
 import { trackTitleFilter } from '@/helpers/filter'
+import { generateTracksListId } from '@/helpers/miscellaneous'
 import { useNavigationSearch } from '@/hooks/useNavigationSearch'
 import { useTracks } from '@/store/library'
 import { defaultStyles } from '@/styles'
@@ -25,7 +26,11 @@ const SongsScreen = () => {
 				contentInsetAdjustmentBehavior="automatic"
 				style={{ paddingHorizontal: screenPadding.horizontal }}
 			>
-				<TracksList tracks={filterSongs} scrollEnabled={false} />
+				<TracksList
+					id={generateTracksListId('songs', search)}
+					tracks={filterSongs}
+					scrollEnabled={false}
+				/>
 			</ScrollView>
 		</View>
 	)
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,11 +1,14 @@
 import { unknownTrackImageUri } from '@/constants/images'
+import { useQueue } from '@/store/queue'
 import { utilsStyles } from '@/styles'
 import FastImage from '@preflower/react-native-web-fast-image'
+import { useRef } from 'react'
 import { FlatList, FlatListProps, Text, View } from 'react-native'
 import TrackPlayer, { Track } from 'react-native-track-player'
 import { TrackListItem } from './TrackListItem'
 
 export type TracksListProps = Partial<FlatListProps<Track>> & {
+	id: string
 	tracks: Track[]
 }
 
@@ -13,10 +16,32 @@ const ItemDivider = () => {
 	return <View style={{ ...utilsStyles.itemSeparator, marginVertical: 9, marginLeft: 60 }} />
 }
 
-export const TracksList = ({ tracks, ...props }: TracksListProps) => {
-	const handleTrackSelect = async (track: Track) => {
-		await TrackPlayer.load(track)
-		await TrackPlayer.play()
+export const TracksList = ({ id, tracks, ...props }: TracksListProps) => {
+	const queueOffset = useRef(0)
+	const { activeQueueId, setActiveQueueId } = useQueue()
+
+	const handleTrackSelect = async (selectedTrack: Track) => {
+		const trackIndex = tracks.findIndex((track) => selectedTrack.url === track.url)
+		if (trackIndex === -1) return
+
+		const isChangingQueue = id !== activeQueueId
+		if (isChangingQueue) {
+			const tracksQueue = tracks.slice(trackIndex).concat(tracks.slice(0, trackIndex))
+
+			await TrackPlayer.reset()
+			await TrackPlayer.add(tracksQueue)
+			await TrackPlayer.play()
+
+			queueOffset.current = trackIndex
+			setActiveQueueId(id)
+		} else {
+			const nextTrackIndex =
+				trackIndex - queueOffset.current < 0
+					? tracks.length + trackIndex - queueOffset.current
+					: trackIndex - queueOffset.current
+			await TrackPlayer.skip(nextTrackIndex)
+			TrackPlayer.play()
+		}
 	}
 
 	return (
