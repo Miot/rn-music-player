d63258108d0c17901ba3e88d25d0be52889d8686
Miot <imuy@foxmail.com>
2025-06-15 17:41:03 +0800

    feat: playbackService

--- src/hooks/useSetupTrackPlayer.tsx
+++ src/hooks/useSetupTrackPlayer.tsx
@@ -1,12 +1,23 @@
 import { useEffect, useRef } from 'react'
-import TrackPlayer, { RepeatMode } from 'react-native-track-player'
+import TrackPlayer, { Capability, RatingType, RepeatMode } from 'react-native-track-player'
 
 const setupPlayer = async () => {
 	await TrackPlayer.setupPlayer({
 		maxCacheSize: 1024 * 10,
 	})
 
-	await TrackPlayer.setVolume(0.3)
+	await TrackPlayer.updateOptions({
+		ratingType: RatingType.Heart,
+		capabilities: [
+			Capability.Play,
+			Capability.Pause,
+			Capability.SkipToNext,
+			Capability.SkipToPrevious,
+			Capability.Stop,
+		],
+	})
+
+	await await TrackPlayer.setVolume(0.3)
 	await TrackPlayer.setRepeatMode(RepeatMode.Queue)
 }
 
